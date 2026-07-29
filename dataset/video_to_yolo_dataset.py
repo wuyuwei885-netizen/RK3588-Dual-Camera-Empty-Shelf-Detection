6085be897a7b6441d2baac97e5412717c38997bb
wuyuwei885-netizen <wuyuwei885@gmail.com>
2026-07-29 10:26:26 +0800

    Add dataset and rectified video files with Git LFS

---
+++ dataset/video_to_yolo_dataset.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bc6e642ce5eb7c850d9bd4012ac50109346d52ebb1db36464c55eb733bdf8eef
+size 17105
---
+++ video_zhengliu/runs_kd_strict_exact/strict_exact_comparison_b24/strict_exact_report.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4ef80c285364371461998cc1a2832c3f52bbdbaa1e18b85d52e0201495a1204f
+size 1598
